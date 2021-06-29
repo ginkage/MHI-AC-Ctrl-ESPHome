193aebba3eba3b2026ee0a95a35ea774841e26a0
Ivan Podogov <ginkage@yandex.ru>
2021-06-29 13:34:10 +0100

    Add "off" to the list of supported modes

--- mhi_ac_ctrl.h
+++ mhi_ac_ctrl.h
@@ -349,7 +349,7 @@ protected:
     {
         auto traits = climate::ClimateTraits();
         traits.set_supports_current_temperature(true);
-        traits.set_supported_modes({ CLIMATE_MODE_HEAT_COOL, CLIMATE_MODE_COOL, CLIMATE_MODE_HEAT, CLIMATE_MODE_DRY, CLIMATE_MODE_FAN_ONLY });
+        traits.set_supported_modes({ CLIMATE_MODE_OFF, CLIMATE_MODE_HEAT_COOL, CLIMATE_MODE_COOL, CLIMATE_MODE_HEAT, CLIMATE_MODE_DRY, CLIMATE_MODE_FAN_ONLY });
         traits.set_supports_two_point_target_temperature(false);
         traits.set_supported_presets({ CLIMATE_PRESET_NONE });
         traits.set_visual_min_temperature(this->minimum_temperature_);
