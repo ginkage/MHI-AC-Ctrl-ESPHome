e74a5d764732f36e9998e8a561a8fe36a71478b5
Robert Jansen <robert@cloudvps.com>
2024-08-28 16:28:14 +0200

    try finxing time imports

--- components/MhiAcCtrl/mhi_ac_ctrl.h
+++ components/MhiAcCtrl/mhi_ac_ctrl.h
@@ -13,6 +13,7 @@ using namespace esphome::climate;
 using namespace esphome::sensor;
 using namespace esphome::text_sensor;
 using namespace esphome::binary_sensor;
+using namespace esphome::time;
 
 static const std::vector<std::string> protection_states = {
     "Normal",
