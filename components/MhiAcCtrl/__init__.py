7c4383811928c459606da7d927289ef55ef9a10a
Robert Jansen <robert@cloudvps.com>
2024-08-28 15:46:07 +0200

    add framesize to init

--- components/MhiAcCtrl/__init__.py
+++ components/MhiAcCtrl/__init__.py
@@ -4,14 +4,17 @@ from esphome.const import CONF_ID
 from esphome.components import climate
 
 AUTO_LOAD = ["sensor", "climate", "text_sensor", "binary_sensor"]
+CONF_FRAME_SIZE = 'frame_size'
 
 mhiacctrl = cg.esphome_ns.namespace('mhiacctrl')
 MhiAcCtrl = cg.global_ns.class_('MhiAcCtrl', cg.Component, climate.Climate)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MhiAcCtrl)
+    cv.Optional(CONF_FRAME_SIZE): cv.declare_id(MhiAcCtrl),
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
+    cg.add(var.set_frame_size(config[CONF_FRAME_SIZE]))
