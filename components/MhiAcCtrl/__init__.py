6e504cf08e7ab434738598f2be3f518c8d9a6423
Robert Jansen <robert@cloudvps.com>
2024-08-28 15:07:31 +0200

    add climate to init

--- components/MhiAcCtrl/__init__.py
+++ components/MhiAcCtrl/__init__.py
@@ -1,9 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.components import climate
+
+AUTO_LOAD = ["sensor", "climate"]
 
 mhiacctrl = cg.esphome_ns.namespace('mhiacctrl')
-MhiAcCtrl = cg.global_ns.class_('MhiAcCtrl', cg.Component)
+MhiAcCtrl = cg.global_ns.class_('MhiAcCtrl', cg.Component, climate.Climat)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MhiAcCtrl)
