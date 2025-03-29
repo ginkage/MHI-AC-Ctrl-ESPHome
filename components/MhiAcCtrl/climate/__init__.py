a9af1387b7ea166795b9617cee7445294a981fcd
Robert Jansen <r.jansen86@gmail.com>
2025-03-29 05:30:50 +0100

    Add icon to climate entity

--- components/MhiAcCtrl/climate/__init__.py
+++ components/MhiAcCtrl/climate/__init__.py
@@ -3,7 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.core import coroutine
 from esphome.const import (
-    CONF_ID
+    CONF_ID,
+    ICON_AIR_CONDITIONER,  # Add this import
 )
 from .. import MhiAcCtrl, CONF_MHI_AC_CTRL_ID
 
@@ -15,14 +16,15 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
         cv.GenerateID(): cv.declare_id(MhiClimate),
         cv.GenerateID(CONF_MHI_AC_CTRL_ID): cv.use_id(MhiAcCtrl),
     }
-).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.COMPONENT_SCHEMA).extend(
+    {
+        cv.Optional("icon", default=ICON_AIR_CONDITIONER): cv.icon,  # Add icon configuration
+    }
+)
 
 
 @coroutine
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     mhi = await cg.get_variable(config[CONF_MHI_AC_CTRL_ID])
-    await cg.register_parented(var, mhi)
-    await cg.register_component(var, config)
-    await climate.register_climate(var, config)
-    
+    cg.add(var.set_icon(config["icon"]))  # Set the icon for the climate entity
