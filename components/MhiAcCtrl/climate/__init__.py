2cd05fb4c19ceca5e2b864325a406786ec549a47
Robert Jansen <r.jansen86@gmail.com>
2025-07-07 15:18:13 +0200

    climate schema

--- components/MhiAcCtrl/climate/__init__.py
+++ components/MhiAcCtrl/climate/__init__.py
@@ -4,22 +4,19 @@ from esphome.components import climate
 from esphome.core import coroutine
 from esphome.const import (
     CONF_ID,
+    CONF_ICON
 )
 from .. import MhiAcCtrl, CONF_MHI_AC_CTRL_ID
 
 mhi_ns = cg.esphome_ns.namespace('mhi')
 MhiClimate = mhi_ns.class_('MhiClimate', cg.Component, climate.Climate)
-ICON_AIR_CONDITIONER = "mdi:air-conditioner"
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+
+CONFIG_SCHEMA = climate.climate_schema(MhiClimate).extend(
     {
-        cv.GenerateID(): cv.declare_id(MhiClimate),
         cv.GenerateID(CONF_MHI_AC_CTRL_ID): cv.use_id(MhiAcCtrl),
+        cv.Optional(CONF_ICON, default="mdi:air-conditioner"): cv.icon,
     }
-).extend(cv.COMPONENT_SCHEMA).extend(
-    {
-        cv.Optional("icon", default=ICON_AIR_CONDITIONER): cv.icon,
-    }
-)
+).extend(cv.COMPONENT_SCHEMA)
 
 
 @coroutine
@@ -29,3 +26,6 @@ async def to_code(config):
     await cg.register_parented(var, mhi)
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
+    
+    if CONF_ICON in config:
+        cg.add(var.set_icon(config[CONF_ICON]))
