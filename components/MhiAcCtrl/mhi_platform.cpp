164c2dc8b266ad41b06021150ff62642e12e6826
Robert Jansen <r.jansen86@gmail.com>
2025-07-16 14:37:08 +0200

    Update nan checking

--- components/MhiAcCtrl/mhi_platform.cpp
+++ components/MhiAcCtrl/mhi_platform.cpp
@@ -92,7 +92,7 @@ void MhiPlatform::set_room_temperature(float value) {
 
 void MhiPlatform::transfer_room_temperature(float value) {
     if (isnan(value)) {
-        if (last_room_temperature_ != NAN) {
+        if (!isnan(this->last_room_temperature_)) {
             ESP_LOGD(TAG, "set room_temp_api: value is NaN, using internal sensor");
             mhi_ac_ctrl_core_.set_troom(0xff); // reset target, use internal sensor
             this->last_room_temperature_ = NAN; // reset last room temperature
