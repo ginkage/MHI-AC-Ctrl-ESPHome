699a3a6a390f0ee3f027103c25debdc73b0439ba
Robert Jansen <r.jansen86@gmail.com>
2026-03-19 21:18:43 +0100

    remove set icon

--- components/MhiAcCtrl/climate/__init__.py
+++ components/MhiAcCtrl/climate/__init__.py
@@ -6,7 +6,6 @@ from esphome.components import climate
 from esphome.core import coroutine
 from esphome.const import (
     CONF_ID,
-    CONF_ICON,
 )
 from .. import MhiAcCtrl, CONF_MHI_AC_CTRL_ID
 
@@ -19,7 +18,6 @@ CONF_VISUAL_MIN_TEMPERATURE = "visual_min_temperature"
 CONFIG_SCHEMA = climate.climate_schema(MhiClimate).extend(
     {
         cv.GenerateID(CONF_MHI_AC_CTRL_ID): cv.use_id(MhiAcCtrl),
-        cv.Optional(CONF_ICON, default="mdi:air-conditioner"): cv.icon,
         cv.Optional(CONF_TEMPERATURE_OFFSET, default=False): cv.boolean,
         cv.Optional(CONF_VISUAL_MIN_TEMPERATURE, default=18.0): cv.temperature,
     }
@@ -34,9 +32,6 @@ async def to_code(config):
     await cg.register_parented(var, mhi)
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-    
-    if CONF_ICON in config:
-        cg.add(var.set_icon(config[CONF_ICON]))
     
     if config[CONF_TEMPERATURE_OFFSET]:
         cg.add(var.set_temperature_offset_enabled(True))
